2402f2403842addcfa9435052e576d27fef34dc5
Thomas PC Maison <tho.polfliet@gmail.com>
2020-08-23 20:47:53 +0200

    Enemy

---
+++ Assets/02_Scripts/Bot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Photon.Pun;
+using Photon.Realtime;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class Bot : MonoBehaviourPun
+{
+    protected Rigidbody botRb;
+    protected NavMeshAgent botNavMeshAgent;
+    public Transform destination;
+
+    private void Awake()
+    {
+        botRb = GetComponent<Rigidbody>();
+        botNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        botNavMeshAgent.SetDestination(destination.position);
+    }
+}
